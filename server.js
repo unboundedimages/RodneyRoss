f76af38b829888308839762e9440b0c25f4436b4
unboundedimages <majid.muhassan@gmail.com>
2018-04-18 11:13:07 -0400

    added nexmo and ejs dependencies

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
-const express= require (express)
-const app = express()
+const express= require ('express');
+const bodyParser= require('body-parser');
+const eyes = require ('ejs');
+const app = express();
+
+app.use(bodyParser.json());
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+app.use(express.static(__dirname+'/views'));
+
+const server = app.listen(5000);
+
+app.get('/', function(){
+	res.render('index');
+})
