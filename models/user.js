1e9ae01317bbe0ebc863f23a8fd38bd10bf80fd9
unboundedimages <majid.muhassan@gmail.com>
2018-04-24 06:19:27 -0400

    update

---
+++ models/user.js
@@ -0,0 +1,31 @@
+// Require bcrypt for hashing
+const bcrypt = require("bcrypt-nodejs");
+
+module.exports = function (sequelize, DataTypes) {
+	let User = sequelize.define("User", {
+
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false, //this means the  field cannot be skipped.
+			unique: true,
+			validate: {
+				isEmail: true
+			}
+		},
+
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false //password can't be blank
+		}
+	});
+
+	//method for User model
+	User.prototype.validPassword = function(password) {
+		return bcrypt.compareSync(password, this.password);
+	};
+
+	User.hook("beforeCreate", function(user){
+		user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+	});
+	return User;
+};
