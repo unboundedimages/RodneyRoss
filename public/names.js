05a08bac77a699d39fcd69d4d83f14a61e89562a
unboundedimages <majid.muhassan@gmail.com>
2018-04-27 15:10:27 -0400

    the pattern attribut was applied to the last name field. It's less code to handle the user putting in invalid characters.

--- public/names.js
+++ public/names.js
@@ -1,7 +1,10 @@
-document.getElementsByClass("names") {
-function alphaOnly(e) {
-	alert(e)
-  var key = e.keyCode;
-  return ((key >= 65 && key <= 90) || key == 8);
-};
+function alphaOnly(event) {
+	// alert(event + "somethingelse")
+  var key = event.keyCode;
+  var formula = (key >= 65 && key <= 90) || key == 8 || key == 189 || key == '' || key == 9 || (key >= 37 && key <= 40) || key == 18 || key == 16  || key == 20 || key == 13; 
+  
+ if (!formula) {
+	alert("you have entered an invalid character.  Please check the field and try again.")
+}
+return (formula);
 };
