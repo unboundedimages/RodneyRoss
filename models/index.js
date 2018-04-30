c4974659493f67a51b537252042dfa178e9a4cae
unboundedimages <majid.muhassan@gmail.com>
2018-04-30 14:36:25 -0400

    organized index.js to keep it consistent with docs

--- models/index.js
+++ models/index.js
@@ -1,36 +1,32 @@
-'use strict';
-
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
+"use strict";
+ 
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
+var env = process.env.NODE_ENV || "development";
 var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
-// var config    = require(__dirname + '/..\/config\/config.json')[env]; //causes throw error 529 
-// var config    = require(__dirname + '/../config/config.json')[env]; // works but is deprecated due to security issue
-var db        = {};
-
-
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-
-
+var sequelize = new Sequelize(config.database, config.username, config.password, config);
+var db = {};
+ 
+ 
 fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+    .readdirSync(__dirname)
+    .filter(function(file) {
+        return (file.indexOf(".") !== 0) && (file !== "index.js");
+    })
+    .forEach(function(file) {
+        var model = sequelize.import(path.join(__dirname, file));
+        db[model.name] = model;
+    });
+ 
+Object.keys(db).forEach(function(modelName) {
+    if ("associate" in db[modelName]) {
+        db[modelName].associate(db);
+    }
 });
-
+ 
+ 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
+ 
 module.exports = db;
