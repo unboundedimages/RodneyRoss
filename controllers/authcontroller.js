f661831a31c87b01d6e9511a710f1ecb69bd2a6d
unboundedimages <majid.muhassan@gmail.com>
2018-04-30 18:41:26 -0400

    added signup.hbs and conntroller for signup in authcontroller

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,7 +1,14 @@
 var exports = module.exports = {}
- 
+ //controller for signup
 exports.signup = function(req, res) {
  
     res.render('signup');
+ 
+}
+
+//controller for sign in
+exports.signin = function(req, res) {
+ 
+    res.render('signin');
  
 }
