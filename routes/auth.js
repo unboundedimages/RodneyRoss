929ab5f7273165b5267927c4190e7bec6706d6f3
unboundedimages <majid.muhassan@gmail.com>
2018-04-30 23:09:50 -0400

    added route for posting signup in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,4 +4,12 @@ var authController = require('../controllers/authcontroller.js');
 module.exports = function(app) {
     app.get('/signup', authController.signup);
     app.get('/signin', authController.signin);
+	//route for posting signgup
+    app.post('/signup', passport.authenticate('local-signup', {
+            successRedirect: '/dashboard',
+     
+            failureRedirect: '/signup'
+        }
+     
+    ));
 }
