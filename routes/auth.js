b9230e4d99a45c86a04e2555cdbd44d0bf35241b
unboundedimages <majid.muhassan@gmail.com>
2018-04-30 23:54:25 -0400

    added logout route in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ var authController = require('../controllers/authcontroller.js');
 module.exports = function(app, passport) {
     app.get('/signup', authController.signup);
     app.get('/signin', authController.signin);
+    app.get('/logout',authController.logout);
 	//route for posting signgup
     app.post('/signup', passport.authenticate('local-signup', {
             successRedirect: '/dashboard',
