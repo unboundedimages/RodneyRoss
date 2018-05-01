c0b130941074fb51fb2d95b137858c06f10a878b
unboundedimages <majid.muhassan@gmail.com>
2018-04-30 23:15:28 -0400

    added arguemnet passport that will be imported in the server.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 //we import the auth controller and define the signup route
 
 var authController = require('../controllers/authcontroller.js');
-module.exports = function(app) {
+module.exports = function(app, passport) {
     app.get('/signup', authController.signup);
     app.get('/signin', authController.signin);
 	//route for posting signgup
