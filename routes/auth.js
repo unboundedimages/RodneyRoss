db41ab7527bab4b1d55765358c0ee7ef0ddb3cef
unboundedimages <majid.muhassan@gmail.com>
2018-04-30 23:59:35 -0400

    added custom middleware to protect route

--- routes/auth.js
+++ routes/auth.js
@@ -5,10 +5,23 @@ module.exports = function(app, passport) {
     app.get('/signup', authController.signup);
     app.get('/signin', authController.signin);
     app.get('/logout',authController.logout);
+    app.get('/dashboard',isLoggedIn, authController.dashboard);
 	//route for posting signgup
     app.post('/signup', passport.authenticate('local-signup', {
             successRedirect: '/dashboard',
             failureRedirect: '/signup'
         }
     ));
+	//custom middleware to protect route
+	function isLoggedIn(req, res, next) {
+	 
+	    if (req.isAuthenticated())
+	     
+	        return next();
+	         
+	    res.redirect('/signin');
+	 
+	}
+
+
 }
