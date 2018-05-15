68d8c20078679abe3ba25644aa6150a6bf38392c
unboundedimages <majid.muhassan@gmail.com>
2018-05-14 21:08:27 -0400

    added code to passport.js in the findOne() callback that allows the updateField to update when user logs in.

--- models/loginLog.js
+++ models/loginLog.js
@@ -1,23 +1,23 @@
 module.exports = function(sequelize, DataTypes) {
- 
-var LoginLog = sequelize.define("loginLog", {
+	var LoginLog = sequelize.define("LoginLog", {
 		time: {
-				type: DataTypes.TIME
+			type: DataTypes.TIME
 		},
-		
+
+		date: {
+			type: DataTypes.DATE
+		},
+
 		last_login: {
 			type: DataTypes.DATE,
 			defaultValue: DataTypes.NOW,
 			unique: true
-			// autoIncrement: true
-		}
- 
-		// date: {
-		// 		type: DataTypes.DATE
-		// }
-});
-LoginLog.associate = function(models){
-	LoginLog.belongsTo(models.user);
-}
-return LoginLog;
+					// autoIncrement: true
+				}
+
+			});
+	LoginLog.associate = function(models){
+		LoginLog.belongsTo(models.user);
+	}
+	return LoginLog;
 }
--- models/user.js
+++ models/user.js
@@ -1,50 +1,53 @@
 module.exports = function(sequelize, Sequelize) {
- 
-    var User = sequelize.define('user', {
- 
-        id: {
-            autoIncrement: true,
-            primaryKey: true,
-            type: Sequelize.INTEGER
-        },
- 
-        firstname: {
-            type: Sequelize.STRING,
-            notEmpty: true
-        },
- 
-        lastname: {
-            type: Sequelize.STRING,
-            notEmpty: true
-        },
- 
-        comment: {
-            type: Sequelize.STRING
-        },
 
-        email: {
-            type: Sequelize.STRING,
-            validate: {
-                isEmail: true
-            }
-        },
- 
-        password: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-		
-        status: {
-            type: Sequelize.ENUM('active', 'inactive'),
-            defaultValue: 'active'
-        }
- 
- 
-    });
-User.associate = function(models){
-	User.hasMany(models.loginLog)
-}
- 
-    return User;
- 
+	var User = sequelize.define('user', {
+
+		id: {
+			autoIncrement: true,
+			primaryKey: true,
+			type: Sequelize.INTEGER
+		},
+
+		firstname: {
+			type: Sequelize.STRING,
+			notEmpty: true
+		},
+
+		lastname: {
+			type: Sequelize.STRING,
+			notEmpty: true
+		},
+
+		comment: {
+			type: Sequelize.STRING
+		},
+
+		email: {
+			type: Sequelize.STRING,
+			validate: {
+				isEmail: true
+			}
+		},
+
+		password: {
+			type: Sequelize.STRING,
+			allowNull: false
+		},
+
+		last_login: {
+			type: Sequelize.DATE,
+			defaultValue: Sequelize.NOW,
+			unique: true
+		},
+
+		status: {
+			type: Sequelize.ENUM('active', 'inactive'),
+			defaultValue: 'active'
+		}
+
+
+	});
+
+	return User;
+
 }
