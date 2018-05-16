864b50aaf0af4d29b26b79631f9fa236e56f8a27
unboundedimages <majid.muhassan@gmail.com>
2018-05-15 21:04:10 -0400

    data now writes to the loginLogs.js file.  Route to  file required in  server.js

--- models/loginLog.js
+++ models/loginLog.js
@@ -25,7 +25,7 @@ module.exports = function(sequelize, DataTypes) {
 
 		LoginLog.belongsTo(models.User, {
 			foreignKey: {
-				allowNull: false
+				allowNull: true
 			}
 		});
 	};
--- server.js
+++ server.js
@@ -74,7 +74,7 @@ function(req, res){
 let models = require("./models");
 
 //routes
-// require('./controllers/logincontroller.js')(app, passport);
+require('./controllers/logincontroller.js')(app);
 //require route from auth.js and pass it through as an arguement.
 let authRoute = require('./routes/auth.js')(app, passport); // this comes from the model.export arguenent in auth.js
 
