e5b0835946d36b7f201289381cf8b36d8c9e9b23
unboundedimages <majid.muhassan@gmail.com>
2018-05-16 20:07:13 -0400

    working on protecting signin route

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,13 +1,26 @@
 var db = require("../models");
+var authController = require('../controllers/authcontroller.js');
+module.exports = function(app, passport) {
+	app.get('/signin', authController.signin);
 
-module.exports = function(app) {
-
-	app.post("/signin", function(req, res) {
-		db.LoginLog.create(req.body).then(function(dbLoginLog) {
+	app.post('/signin', passport.authenticate('local-signin', { //route for posting to /signin.
+		function(req, res) {
+			db.LoginLog.create(req.body).then(function(dbLoginLog) {
 			// res.json(dbLoginLog);
 			res.render('Port')
 		});
-	});
+	// }
+	successRedirect: '/Port',
+	failureRedirect: '/signin',
+}
+));
+	// .then(function(req, res) {
+	// 	db.LoginLog.create(req.body).then(function(dbLoginLog) {
+	// 		// res.json(dbLoginLog);
+	// 		res.render('Port')
+	// 	});
+	// });
+	
 
 	app.put("/sigin", function(req, res) {
 		db.LoginLog.update(
@@ -21,4 +34,12 @@ module.exports = function(app) {
 			res.render('Port')
 		});
 	});
+
+		//custom middleware to protect route
+		function isLoggedIn(req, res, next) {
+			if (req.isAuthenticated())
+			return next();
+		res.redirect('/signin');
+	}
+
 }
--- server.js
+++ server.js
@@ -74,7 +74,7 @@ function(req, res){
 let models = require("./models");
 
 //routes
-require('./routes/loginRoute.js')(app);
+require('./routes/loginRoute.js')(app, passport);
 //require route from auth.js and pass it through as an arguement.
 let authRoute = require('./routes/auth.js')(app, passport); // this comes from the model.export arguenent in auth.js
 
@@ -82,7 +82,7 @@ let authRoute = require('./routes/auth.js')(app, passport); // this comes from t
 require('./config/passport/passport.js')(passport, models.user, models.loginLog);
 
 // Sync to database
-models.sequelize.sync({force: false}).then(function(){ //this line is relative to user.js in the models folder setting the value to true will drop the db/table
+models.sequelize.sync({force: true}).then(function(){ //this line is relative to user.js in the models folder setting the value to true will drop the db/table
 	database:"process.env.dbn"
 	app.listen(PORT, function() {
 		console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
