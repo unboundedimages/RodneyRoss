54f68d82a05c771a026e3cb053664da1079fed93
unboundedimages <majid.muhassan@gmail.com>
2018-05-23 03:13:20 -0400

    working on connecting click handle events with the signin process for alerts.

--- public/logic/names.js
+++ public/logic/names.js
@@ -3,7 +3,7 @@ function alphaOnly(event) {
   var key = event.keyCode;
   var formula = (key >= 65 && key <= 90) || key == 8 || key == 189 || key == '' || key == 9 || (key >= 37 && key <= 40) || key == 18 || key == 16  || key == 20 || key == 13; 
   
- if (!formula) {
+  if (!formula) {
 	alert("you have entered an invalid character.  Please check the field and try again.")
 }
 return (formula);
