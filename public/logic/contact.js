9edfd0a0ad6f1dc425a08d889f2d59cf1771f548
unboundedimages <majid.muhassan@gmail.com>
2018-06-04 14:07:44 -0400

    changed the layout for the phone button toggle.

--- public/logic/contact.js
+++ public/logic/contact.js
@@ -2,27 +2,23 @@
 //displays phone number sticky
 (call_me = () => {
 	let phoneBtn = document.getElementById('phone_btn');
+	let joke = document.getElementById("joke");
 	let imgs = document.getElementById("call_me");
 	let imgs2 = document.getElementById("bak_bak");
 	let hideStarburst = document.getElementById("starburst");
 	let hidehow2 = document.getElementById("how2");
 	(phoneBtn.onclick = () => {
 		
-		if (hideStarburst.style.display === "block" && hidehow2.style.display === "block") {
+		if (joke.style.display === "none") {
 			
-			imgs.style.display = "block";
-			imgs2.style.display = "block";
-			hideStarburst.style.display = "none";
-			hidehow2.style.display = "none";
+			joke.style.display ="block";
 			
 		} else { 
 
-			imgs.style.display = "none";
-			imgs2.style.display = "none";
-			hideStarburst.style.display = "block";
-			hidehow2.style.display = "block";
+			joke.style.display ="none";
 		}
 	})();
+
 	let emailBtn = document.getElementById('email_btn');
 	// let hideBakBak = document.getElementById('bak_bak');
 	emailBtn.onclick = () => {
