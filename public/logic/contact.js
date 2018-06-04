edf3cc0d71d0f7f4d74bf7145c8d0cf51bd6a43c
unboundedimages <majid.muhassan@gmail.com>
2018-06-04 12:31:27 -0400

    added toggle feature to the phone button

--- public/logic/contact.js
+++ public/logic/contact.js
@@ -6,12 +6,23 @@
 	let imgs2 = document.getElementById("bak_bak");
 	let hideStarburst = document.getElementById("starburst");
 	let hidehow2 = document.getElementById("how2");
-	phoneBtn.onclick = () => {
-		imgs.style.display = "block";
-		imgs2.style.display = "block";
-		hideStarburst.style.display = "none";
-		hidehow2.style.display = "none";
-	}
+	(phoneBtn.onclick = () => {
+		
+		if (hideStarburst.style.display === "block" && hidehow2.style.display === "block") {
+			
+			imgs.style.display = "block";
+			imgs2.style.display = "block";
+			hideStarburst.style.display = "none";
+			hidehow2.style.display = "none";
+			
+		} else { 
+
+			imgs.style.display = "none";
+			imgs2.style.display = "none";
+			hideStarburst.style.display = "block";
+			hidehow2.style.display = "block";
+		}
+	})();
 	let emailBtn = document.getElementById('email_btn');
 	// let hideBakBak = document.getElementById('bak_bak');
 	emailBtn.onclick = () => {
