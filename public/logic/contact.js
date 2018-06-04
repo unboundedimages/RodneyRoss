32529f9473434f0fe5e201496756421abee489f2
unboundedimages <majid.muhassan@gmail.com>
2018-06-03 20:43:14 -0400

    added image under call me image to remove negative space

--- public/logic/contact.js
+++ public/logic/contact.js
@@ -2,11 +2,13 @@
 //displays phone number sticky
 (call_me = () => {
 	let phoneBtn = document.getElementById('phone_btn');
-	let  imgs = document.getElementById("call_me", "bak_bak");
+	let imgs = document.getElementById("call_me");
+	let imgs2 = document.getElementById("bak_bak");
 	let hideStarburst = document.getElementById("starburst");
 	let hidehow2 = document.getElementById("how2");
 	phoneBtn.onclick = () => {
 		imgs.style.display = "block";
+		imgs2.style.display = "block";
 		hideStarburst.style.display = "none";
 		hidehow2.style.display = "none";
 	}
